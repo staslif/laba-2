341e00f0eaf17eb243e2c2b146d54018b0cab76d
staslif <149203375+staslif@users.noreply.github.com>
2023-10-28 12:58:11 +0300

    Add files via upload

---
+++ laba_2.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите тариф (минут)");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Плата");
+int X = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество проговоренных минут");
+int B = Convert.ToInt32(Console.ReadLine());
+if (A >= B)
+{
+    Console.WriteLine($"Плата {X}");
+}
+else if (A < B)
+{
+    Console.WriteLine("Плата за использование сверх тарифа");
+    int V = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Плата {V}");
+}
+
+
+
