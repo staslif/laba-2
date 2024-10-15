97b043c421a9de9db46e83f7969a3bb06f3ebb48
staslif <149203375+staslif@users.noreply.github.com>
2024-10-15 12:32:06 +0300

    Update laba_2.cs шоколадки

--- laba_2.cs
+++ laba_2.cs
@@ -1,12 +1,41 @@
-﻿Console.WriteLine("Введите тариф (минут)");
-int A = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Плата");
-int X = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите количество проговоренных минут");
-int B = Convert.ToInt32(Console.ReadLine());
-if (A >= B)
-{
-    Console.WriteLine($"Плата {X}");
-}
+using System;
 
+class ChocolateCalculator
+{
+    static void Main()
+    {
+        // Запрос данных у пользователя
+        Console.WriteLine("Пожалуйста, укажите сумму денег: ");
+        int totalFunds = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Какова стоимость одной шоколадки? ");
+        int chocolateCost = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Сколько оберток нужно для обмена на новую шоколадку? ");
+        int wrappersNeeded = Convert.ToInt32(Console.ReadLine());
+
+        // Получение результата
+        int totalChocolates = CalculateMaxChocolates(totalFunds, chocolateCost, wrappersNeeded);
+
+        // Вывод результата
+        Console.WriteLine($"Вы сможете купить максимально {totalChocolates} шоколадок.");
+    }
+
+    // Метод для вычисления максимального количества шоколадок
+    static int CalculateMaxChocolates(int funds, int cost, int wrappersExchange)
+    {
+        int purchasedChocolates = funds / cost;
+        int currentWrappers = purchasedChocolates;
+
+        // Обмен оберток на шоколадки
+        while (currentWrappers >= wrappersExchange)
+        {
+            int newChocolates = currentWrappers / wrappersExchange;
+            purchasedChocolates += newChocolates;
+            currentWrappers = newChocolates + (currentWrappers % wrappersExchange);
+        }
+
+        return purchasedChocolates;
+    }
+}
 
