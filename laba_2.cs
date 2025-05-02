c8e1ab4da12f0669d51272c86d23ea23fddd83a7
staslif <149203375+staslif@users.noreply.github.com>
2025-05-02 08:13:37 +0300

    Update laba_2.cs

--- laba_2.cs
+++ laba_2.cs
@@ -1,41 +1,66 @@
 using System;
+using System.IO;
+using System.Linq;
 
-class ChocolateCalculator
+class Program
 {
-    static void Main()
+    static void Main(string[] args)
     {
-        // Запрос данных у пользователя
-        Console.WriteLine("Пожалуйста, укажите сумму денег: ");
-        int totalFunds = Convert.ToInt32(Console.ReadLine());
+        // Читаем все строки из файла "17.txt" и преобразуем их в массив целых чисел
+        int[] integerNumbers = File.ReadAllLines("17.txt").Select(int.Parse).ToArray();
 
-        Console.WriteLine("Какова стоимость одной шоколадки? ");
-        int chocolateCost = Convert.ToInt32(Console.ReadLine());
+        // Находим минимальное трехзначное число (>=100), которое делится на 7
+        int minimumThreeDigitDivisibleBy7 = integerNumbers.Where(num => num >= 100 && num % 7 == 0).DefaultIfEmpty(0).Min();
 
-        Console.WriteLine("Сколько оберток нужно для обмена на новую шоколадку? ");
-        int wrappersNeeded = Convert.ToInt32(Console.ReadLine());
+        // Находим минимальное четырехзначное число (>=1000)
+        int minimumFourDigit = integerNumbers.Where(num => num >= 1000).DefaultIfEmpty(0).Min();
 
-        // Получение результата
-        int totalChocolates = CalculateMaxChocolates(totalFunds, chocolateCost, wrappersNeeded);
+        // Вычисляем последнюю цифру минимального четырехзначного числа
+        int lastDigitOfMinimumFourDigit = Math.Abs(minimumFourDigit % 10);
 
-        // Вывод результата
-        Console.WriteLine($"Вы сможете купить максимально {totalChocolates} шоколадок.");
-    }
+        // Инициализируем счетчик подходящих пар
+        int validPairCount = 0;
 
-    // Метод для вычисления максимального количества шоколадок
-    static int CalculateMaxChocolates(int funds, int cost, int wrappersExchange)
-    {
-        int purchasedChocolates = funds / cost;
-        int currentWrappers = purchasedChocolates;
+        // Инициализируем переменную для хранения максимальной суммы элементов пар
+        int maximumSumOfPairs = int.MinValue;
 
-        // Обмен оберток на шоколадки
-        while (currentWrappers >= wrappersExchange)
+        // Проходим по всем парам подряд идущих чисел в массиве
+        for (int index = 0; index < integerNumbers.Length - 1; index++)
         {
-            int newChocolates = currentWrappers / wrappersExchange;
-            purchasedChocolates += newChocolates;
-            currentWrappers = newChocolates + (currentWrappers % wrappersExchange);
+            // Первое число в паре
+            int firstNumber = integerNumbers[index];
+
+            // Второе число в паре
+            int secondNumber = integerNumbers[index + 1];
+
+            // Проверяем условие: хотя бы одно число в паре меньше minimumThreeDigitDivisibleBy7
+            if (firstNumber < minimumThreeDigitDivisibleBy7 || secondNumber < minimumThreeDigitDivisibleBy7)
+            {
+                // Вычисляем произведение чисел в паре
+                int productOfPair = firstNumber * secondNumber;
+
+                // Находим последнюю цифру произведения
+                int lastDigitOfProduct = Math.Abs(productOfPair % 10);
+
+                // Проверяем, совпадает ли последняя цифра произведения с последней цифрой minimumFourDigit
+                if (lastDigitOfProduct == lastDigitOfMinimumFourDigit)
+                {
+                    // Увеличиваем счетчик подходящих пар
+                    validPairCount++;
+
+                    // Вычисляем сумму чисел в паре
+                    int sumOfPair = firstNumber + secondNumber;
+
+                    // Обновляем максимальную сумму, если текущая сумма больше
+                    if (sumOfPair > maximumSumOfPairs)
+                    {
+                        maximumSumOfPairs = sumOfPair;
+                    }
+                }
+            }
         }
 
-        return purchasedChocolates;
+        // Выводим результат: количество подходящих пар и максимальную сумму
+        Console.WriteLine($"{validPairCount} {maximumSumOfPairs}");
     }
 }
-
